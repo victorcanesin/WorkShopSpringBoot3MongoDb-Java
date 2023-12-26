fc937aac2b6fa395982ac09f1a5b28371ab06716
Victor Canesin <victorcanesin.rx@gmail.com>
2023-12-26 11:14:11 -0300

    Dto user

--- src/main/java/com/canesin/workshopmongo/resources/UserResource.java
+++ src/main/java/com/canesin/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.canesin.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.canesin.workshopmongo.domain.User;
+import com.canesin.workshopmongo.dto.UserDTO;
 import com.canesin.workshopmongo.services.UserService;
 
 @RestController // indica que sera o controlador
@@ -19,10 +21,11 @@ public class UserResource {
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> lista = service.findAll();
+		List<UserDTO> listaDTO = lista.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok()
-							 .body(lista);
+							 .body(listaDTO);
 	}
 
 }
