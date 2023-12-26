880e9b730231febb8ca5ce12454be3691b3d3a58
Victor Canesin <victorcanesin.rx@gmail.com>
2023-12-26 14:03:51 -0300

    post user

--- src/main/java/com/canesin/workshopmongo/resources/UserResource.java
+++ src/main/java/com/canesin/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.canesin.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.canesin.workshopmongo.domain.User;
 import com.canesin.workshopmongo.dto.UserDTO;
@@ -17,28 +21,33 @@ import com.canesin.workshopmongo.services.UserService;
 @RestController // indica que sera o controlador
 @RequestMapping(value = "/users") // url para invocar
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
 
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
-		
+
 		List<User> lista = service.findAll();
 		List<UserDTO> listaDTO = lista.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-		
-		return ResponseEntity.ok()
-							 .body(listaDTO);
+
+		return ResponseEntity.ok().body(listaDTO);
 	}
-	
-	@GetMapping(value="/{id}")
+
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
-		
-		User user = service.findById(id);		
-		
-		return ResponseEntity.ok()
-							 .body(new UserDTO(user));
+
+		User user = service.findById(id);
+
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO dto) {
+		User user = service.fromDTO(dto);
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 
 }
--- src/main/java/com/canesin/workshopmongo/services/UserService.java
+++ src/main/java/com/canesin/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.canesin.workshopmongo.domain.User;
+import com.canesin.workshopmongo.dto.UserDTO;
 import com.canesin.workshopmongo.repository.UserRepository;
 import com.canesin.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -27,4 +28,12 @@ public class UserService {
 		// orElseThrow > vai tentar dar o get, se nao tiver usuario estoura a exception	
 	}
 
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO dto) {
+		return new User(dto.getId(), dto.getName(), dto.getEmail());
+	}
+	
 }
