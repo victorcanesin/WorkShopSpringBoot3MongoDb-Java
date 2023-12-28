ff5f69c7990b075c766db1831fdadb3c5a1b3b69
Victor Canesin <victorcanesin.rx@gmail.com>
2023-12-28 08:37:15 -0300

    query simples na mão

--- src/main/java/com/canesin/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/canesin/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.canesin.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.canesin.workshopmongo.domain.Post;
@@ -11,4 +12,10 @@ import com.canesin.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>  {
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	
+	
 }
