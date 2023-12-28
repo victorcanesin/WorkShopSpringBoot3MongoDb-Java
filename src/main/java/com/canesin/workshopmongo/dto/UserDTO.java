ae1ad44ddcd3cc51e06852ae775c7f7fcf4f1e0d
Victor Canesin <victorcanesin.rx@gmail.com>
2023-12-28 07:57:07 -0300

    get posts

--- src/main/java/com/canesin/workshopmongo/dto/UserDTO.java
+++ src/main/java/com/canesin/workshopmongo/dto/UserDTO.java
@@ -1,12 +1,7 @@
 package com.canesin.workshopmongo.dto;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.springframework.data.mongodb.core.mapping.DBRef;
-
-import com.canesin.workshopmongo.domain.Post;
 import com.canesin.workshopmongo.domain.User;
 
 public class UserDTO  implements Serializable{
--- src/main/java/com/canesin/workshopmongo/resources/UserResource.java
+++ src/main/java/com/canesin/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.canesin.workshopmongo.domain.Post;
 import com.canesin.workshopmongo.domain.User;
 import com.canesin.workshopmongo.dto.UserDTO;
 import com.canesin.workshopmongo.services.UserService;
@@ -42,6 +43,14 @@ public class UserResource {
 		User user = service.findById(id);
 
 		return ResponseEntity.ok().body(new UserDTO(user));
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> getPosts(@PathVariable String id) {
+
+		User user = service.findById(id);
+
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 
 	@PostMapping
